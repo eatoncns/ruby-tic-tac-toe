b35d730e713e800c1ef1fae6d7df3a348c9b36ba
Philip Meier <philip.meier06@googlemail.com>
2017-07-26 11:38:06 +0100

    Add play again prompt input validation

--- lib/prompt.rb
+++ lib/prompt.rb
@@ -1,7 +1,10 @@
 class Prompt
   def self.play_again?(input = $stdin, output = $stdout)
-    output.print("Do you want to play another game? (y/n): ")
-    answer = input.gets.chomp
+    answer = nil
+    until answer == "y" || answer == "n" do
+      output.print("Do you want to play another game? (y/n): ")
+      answer = input.gets.chomp
+    end
     answer == "y"
   end
 end
--- spec/prompt_spec.rb
+++ spec/prompt_spec.rb
@@ -2,7 +2,8 @@ require 'prompt'
 
 describe Prompt do
   describe ".play_again?" do
-    let(:input) { StringIO.new("y\n") }
+    let(:yes_answer) { "y\n" }
+    let(:input) { StringIO.new(yes_answer) }
     let(:output) { StringIO.new }
     
     it "outputs question" do
@@ -13,6 +14,29 @@ describe Prompt do
     context "when player answers y" do
       it "returns true" do
         expect(Prompt.play_again?(input, output)).to be true
+      end
+    end
+
+    context "when player answers n" do
+      it "returns false" do
+        input = StringIO.new("n\n")
+        expect(Prompt.play_again?(input, output)).to be false
+      end
+    end
+
+    ["alfda", "1", "yes"].each do |answer|
+      context "when player gives invalid answer #{answer}" do
+        it "outputs question again" do
+          input = StringIO.new(answer + "\n" + yes_answer)
+          Prompt.play_again?(input, output)
+          times_asked = output.string.scan("Do you want to play another game? (y/n): ").size
+          expect(times_asked).to eq 2
+        end
+
+        it "returns eventual valid answer" do
+          input = StringIO.new(answer + "\n" + yes_answer)
+          expect(Prompt.play_again?(input, output)).to be true
+        end
       end
     end
   end
