b36cf0f403a219a0268f9c4d748188b558140ce2
Philip Meier <philip.meier06@googlemail.com>
2017-07-27 13:15:23 +0100

    Add constants for different game modes

--- lib/game_mode.rb
+++ lib/game_mode.rb
@@ -2,12 +2,15 @@ require_relative 'prompt'
 require_relative 'human_vs_computer'
 
 class GameMode
+  HUMAN_VS_COMPUTER = 1
+  HUMAN_VS_HUMAN = 2
+
   def self.select(console)
     console.puts("Game modes:\n" +
                  "  1. Human vs Computer\n" +
                  "  2. Human vs Human\n")
     mode = Prompt.for_int_in_range(console, "Select game mode", (1..2))
-    if mode == 1
+    if mode == HUMAN_VS_COMPUTER
       HumanVsComputer.new
     else
       HumanVsHuman.new
