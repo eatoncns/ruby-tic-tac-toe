238d23ed006753f392429edd7bd98d623ff19d9c
Philip Meier <philip.meier06@googlemail.com>
2017-07-31 16:42:19 +0100

    Update negamax to handle drawn terminal state

--- lib/negamax.rb
+++ lib/negamax.rb
@@ -2,6 +2,9 @@ class Negamax
   MAX_VALUE = 100
   
   def self.calculate(board, nextPlayerMark)
+    if board.drawn?
+      return 0
+    end
     winning_mark = board.winning_mark()
     if winning_mark == nextPlayerMark
       MAX_VALUE
--- spec/negamax_spec.rb
+++ spec/negamax_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Negamax do
                            "", "", ""])
     expect(Negamax.calculate(board, "O")).to eq -Negamax::MAX_VALUE
   end
+
+  it "returns zero when board is drawn" do
+    board = Board.from_a(["X", "X", "O",
+                          "O", "X", "X",
+                          "X", "O", "O"])
+    expect(Negamax.calculate(board, "X")).to eq 0
+    expect(Negamax.calculate(board, "O")).to eq 0 
+  end
 end
