1ab6c87e474e00b0d5ebd98965061388859f4a36
Philip Meier <philip.meier06@googlemail.com>
2017-08-01 10:27:07 +0100

    Update negamax algorithm to take into account number of moves

--- lib/negamax.rb
+++ lib/negamax.rb
@@ -1,15 +1,48 @@
 class Negamax
   MAX_VALUE = 100
+  VALUE_BOUND = MAX_VALUE + 1
   
   def self.value_to_mark(mark, board)
-    if board.drawn?
-      return 0
-    end
-    winning_mark = board.winning_mark()
-    if winning_mark == mark
-      MAX_VALUE
-    else
-      -MAX_VALUE
-    end
+    value_to_mark_at(mark, board, 0)
   end
+
+  private
+    def self.value_to_mark_at(mark, board, depth)
+      if board.game_over?
+        terminal_value(mark, board, depth)
+      else
+        max_next_move_value(mark, board, depth)
+      end
+    end
+
+    def self.terminal_value(mark, board, depth)
+      if board.drawn?
+        return 0
+      end
+      winning_mark = board.winning_mark()
+      if winning_mark == mark
+        return MAX_VALUE - depth
+      end
+      -MAX_VALUE + depth
+    end
+
+    def self.max_next_move_value(mark, board, depth)
+      max_value = -VALUE_BOUND
+      board.empty_spaces.each do |space|
+        move_value = value_of_move(mark, board, space, depth)
+        max_value = [max_value, move_value].max
+      end
+      max_value
+    end
+
+    def self.value_of_move(mark, board, space, depth)
+      board.set_mark(space, mark)
+      value = -value_to_mark_at(opponent(mark), board, depth+1)
+      board.set_mark(space, "")
+      value
+    end
+
+    def self.opponent(mark)
+      (mark == "X") ? "O" : "X"
+    end
 end
--- spec/negamax_spec.rb
+++ spec/negamax_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe Negamax do
     expect(Negamax.value_to_mark("X", board)).to eq 0
     expect(Negamax.value_to_mark("O", board)).to eq 0 
   end
+
+  it "subtracts number of marks needed to end game from winning score" do
+    board = Board.from_a(["X", "X", "",
+                          "O", "O", "",
+                           "", "", ""])
+    expect(Negamax.value_to_mark("X", board)).to eq Negamax::MAX_VALUE - 1
+  end
+
+  it "add number of marks needed to end game to losing score" do
+    board = Board.from_a(["", "", "O",
+                          "O", "X", "",
+                          "X", "", "X"])
+    expect(Negamax.value_to_mark("O", board)).to eq -Negamax::MAX_VALUE + 2
+  end
 end
