f9a28baa6ce7030db874dc1ec90b6120199bc6bb
Philip Meier <philip.meier06@googlemail.com>
2017-08-01 13:55:32 +0100

    Update computer to use negamax instead of random space choice

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,11 +1,12 @@
+require_relative 'negamax'
+
 class Computer
   attr_reader :mark
   
-  def initialize(mark, name, console, random = Random.new)
+  def initialize(mark, name, console)
     @mark = mark
     @name = name
     @console = console
-    @random = random
   end
 
   def choose_space(board)
@@ -15,10 +16,23 @@ class Computer
   end
 
   private
+    Move = Struct.new(:space, :value)
+
     def get_space(board)
-      empty_spaces = board.empty_spaces
-      random_index = @random.rand(0..empty_spaces.length-1)
-      empty_spaces[random_index]
+      move_values = board.empty_spaces.collect { |space| Move.new(space, value_of_move(board, space)) }
+      best_move = move_values.max { |a, b| a.value <=> b.value }
+      best_move.space
+    end
+
+    def value_of_move(board, space)
+      board.set_mark(space, @mark)
+      value = -Negamax.value_to_mark(opponent(@mark), board)
+      board.remove_mark(space)
+      value
+    end 
+
+    def opponent(mark)
+      (mark == "X") ? "O" : "X"
     end
 
     def output_choice(space)
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -4,21 +4,43 @@ require 'console'
 
 RSpec.describe Computer do
   let(:console) { instance_double("Console", :puts => nil, :line_break => nil) }
-  let(:random) { instance_double("Random", :rand => 1) }
-  let(:computer) { Computer.new("X", "Bran", console, random) }
-  let(:board) { Board.from_a(["X", "", "O",
-                              "", "X", "O",
-                              "X", "", ""]) }
+  let(:computer) { Computer.new("X", "Bran", console) }
 
   describe "#choose_space" do
-    it "selects a random empty space" do
-      expect(random).to receive(:rand).with(0..3).and_return(1)
-      expect(computer.choose_space(board)).to eq 4
-    end
-
     it "outputs chosen space" do
+      board = Board.from_a(["X", "O", "O",
+                             "", "X", "X",
+                            "O", "X", "O"])
       expect(console).to receive(:puts).with("[Bran] Chooses space 4")
       computer.choose_space(board)
+    end
+
+    it "chooses winning space when possible" do
+      board = Board.from_a(["X", "", "X",
+                            "O", "", "",
+                            "O", "", ""])
+      expect(computer.choose_space(board)).to eq 2
+    end
+
+    it "blocks opponent win" do
+      board = Board.from_a(["O", "X", "X",
+                            "O", "", "",
+                             "", "", ""])
+      expect(computer.choose_space(board)).to eq 7
+    end
+
+    it "blocks a fork" do
+      board = Board.from_a(["O", "", "",
+                            "", "X", "",
+                            "", "", "O"])
+      expect(computer.choose_space(board)).to satisfy("be an edge space") { |space| space.even? }
+    end
+
+    it "creates fork" do
+      board = Board.from_a(["O", "", "",
+                            "", "X", "",
+                            "", "O", "X"])
+      expect(computer.choose_space(board)).to eq 3 
     end
   end
 end
