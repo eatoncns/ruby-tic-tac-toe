d742a20036151c009af9353f69813be4a8b84467
Philip Meier <philip.meier06@googlemail.com>
2017-08-02 13:49:59 +0100

    Add a wrapper class for computer player
    This will be used to allow player to step through a computer vs
    computer game one move at a time

---
+++ lib/interactive_computer.rb
@@ -0,0 +1,24 @@
+require 'board_display'
+
+class InteractiveComputer
+  def initialize(computer, console)
+    @computer = computer
+    @console = console
+  end
+
+  def choose_space(board)
+    display_board(board)
+    wait_for_user()
+    @computer.choose_space(board)
+  end
+
+  def display_board(board)
+    @console.puts(BoardDisplay.as_string(board))
+  end
+
+  def wait_for_user
+    @console.line_break
+    @console.print("Press enter to continue")
+    @console.get_string()
+  end
+end
---
+++ spec/interactive_computer_spec.rb
@@ -0,0 +1,35 @@
+require 'interactive_computer'
+require 'console'
+require 'board'
+
+RSpec.describe InteractiveComputer do
+  let(:computer) { instance_double("Computer", { :mark => "X", :choose_space => 1 }) }
+  let(:console) { instance_double("Console", { :puts => nil, :print => nil, :line_break => nil, :get_string => ""}) }
+  let(:interactive_computer) { InteractiveComputer.new(computer, console) }
+  let(:board) { Board.new }
+  
+  describe "#choose_space" do
+    it "outputs pre-move board state" do
+      board_output = "|---|---|---|\n" +
+                     "| 1 | 2 | 3 |\n" +
+                     "|---|---|---|\n" +
+                     "| 4 | 5 | 6 |\n" +
+                     "|---|---|---|\n" +
+                     "| 7 | 8 | 9 |\n" +
+                     "|---|---|---|\n"
+      expect(console).to receive(:puts).with(board_output)
+      interactive_computer.choose_space(board)
+    end
+
+    it "prompts user to continue" do
+      expect(console).to receive(:print).with("Press enter to continue").ordered
+      expect(console).to receive(:get_string).ordered
+      interactive_computer.choose_space(board)
+    end
+
+    it "returns space from wrapped computer" do
+      expect(computer).to receive(:choose_space).and_return(5)
+      expect(interactive_computer.choose_space(board)).to eq 5
+    end
+  end
+end
