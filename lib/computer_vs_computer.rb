2d8d69e4addde784e093bd719047af6c2418047f
Philip Meier <philip.meier06@googlemail.com>
2017-08-02 14:16:10 +0100

    Add configuration class for computer vs computer players

---
+++ lib/computer_vs_computer.rb
@@ -0,0 +1,14 @@
+require 'computer'
+require 'interactive_computer'
+
+class ComputerVsComputer
+  def initialize(console)
+    @console = console
+  end
+
+  def configure
+    comp_one = Computer.new("X", "Ying", @console)
+    comp_two = Computer.new("O", "Yang", @console)
+    [InteractiveComputer.new(comp_one, @console), InteractiveComputer.new(comp_two, @console)]
+  end
+end
