a57b829e2309ae7814a97cc58d42107eebfc26de
Philip Meier <philip.meier06@googlemail.com>
2017-08-03 14:23:35 +0100

    Add class for handling selection of board size

---
+++ lib/board_size.rb
@@ -0,0 +1,22 @@
+require_relative 'prompt'
+require_relative 'board'
+
+class BoardSize
+  def initialize(console)
+    @console = console
+    @prompt = Prompt.new(console)
+  end
+
+  def select
+    size = get_size()
+    Board.new(size)
+  end
+
+  def get_size
+    @console.puts("Board sizes:\n" + 
+                  "  1. 3x3\n" +
+                  "  2. 4x4\n")
+    selection = @prompt.for_int_in_range("Select board size", (1..2))
+    (selection == 1) ? 3 : 4
+  end
+end
---
+++ spec/board_size_spec.rb
@@ -0,0 +1,37 @@
+require 'board_size'
+require 'console'
+
+RSpec.describe BoardSize do
+  let(:console) { instance_double("Console", { :puts => nil, :print => nil, :get_int => 1 }) }
+  let(:board_size) { BoardSize.new(console) }
+
+  it "outputs board size options" do
+    options = "Board sizes:\n" +
+              "  1. 3x3\n" +
+              "  2. 4x4\n"
+    expect(console).to receive(:puts).with(options)
+    board_size.select() 
+  end
+
+  it "prompts for selection" do
+    prompt = "Select board size (1-2): "
+    expect(console).to receive(:print).with(prompt)
+    board_size.select()
+  end
+
+  context "when player selects 3x3" do
+    it "returns board with dimension 3" do
+      allow(console).to receive(:get_int).and_return(1)
+      board = board_size.select()
+      expect(board.dimension).to eq 3
+    end
+  end
+  
+  context "when player selects 4x4" do
+    it "returns board with dimension 4" do
+      allow(console).to receive(:get_int).and_return(2)
+      board = board_size.select()
+      expect(board.dimension).to eq 4
+    end
+  end
+end
